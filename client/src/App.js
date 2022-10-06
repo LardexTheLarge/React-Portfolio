a7dad3d6d31f22a3b684da72dec4aae0fc31c515
Gabe Vee <0gabevee0@gmail.com>
2022-10-06 16:47:28 -0600

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+// First we import our Hello component from our components folder.
+import Hello from "./components/Hello";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+
+// React apps typically have a single App component at the very top that can reference other React components.
+// This component, `App`, is our main component that is importing `Hello` and rendering it in the return method.
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Hello />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/components/Footer.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <h1>THis is my footer</h1>
+      <h2>My favorite food</h2>
+      <ul>
+        <li>Pizza</li>
+        <li>Burritos</li>
+        <li>Cheese</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Footer;
